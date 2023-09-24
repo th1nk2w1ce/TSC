48fe26517eaab8c4e0266d2cd141303a42d0922f
Andrey Burnosov <andreyburnosov24@gmail.com>
2023-09-24 18:58:10 +0300

    fix balance formating

--- main.py
+++ main.py
@@ -219,7 +219,7 @@ async def personal_account(message: types.Message):
         referer_name = (await bot.get_chat(referer)).first_name
         referer = f'[{referer_name}](tg://user?id={referer})'
     
-    await bot.send_message(chat_id=message.from_user.id, text=f'Рефералы первого уровня: {firts_lvl_referals}\nВсе рефералы: {all_referals}\nВас пригласил: {referer}\nБаланс STS: {sts}\nРеферальная ссылка: {link}'.replace('.', '\\.'), parse_mode='MarkdownV2')
+    await bot.send_message(chat_id=message.from_user.id, text=f'Рефералы первого уровня: {firts_lvl_referals}\nВсе рефералы: {all_referals}\nВас пригласил: {referer}\nБаланс STS: {sts:.2f}\nРеферальная ссылка: {link}'.replace('.', '\\.'), parse_mode='MarkdownV2')
 
 # Entry point for the application; starts polling for updates from the Telegram API
 if __name__ == '__main__':
