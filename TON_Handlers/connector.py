5e8db78f5ba6963d4677c775489af2f9b49c450a
kkotlyarenko <kirill07022006@mail.ru>
2024-05-16 11:47:40 +0300

    fix

--- TON_Handlers/connector.py
+++ TON_Handlers/connector.py
@@ -8,5 +8,5 @@ Manifest_Url = config.manifest_url.get_secret_value()
 
 def get_connector(chat_id: int):
     connector = TonConnect(Manifest_Url, storage=TcStorage(chat_id))
-    connector.api_tokens = {"tonapi": config.tonapi_bridge_key}
+    connector.api_tokens = {"tonapi": config.tonapi_bridge_key.get_secret_value()}
     return connector
